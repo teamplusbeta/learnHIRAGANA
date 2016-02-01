ff400273a027de84991bd703f59e055352a5a504
teamplusbeta <developer@teamplusbeta.net>
2016-02-01 17:08:09 +0900

    Deleted waste codes and modified some codes.

--- app/src/main/java/net/teamplusbeta/learnhiragana/MainActivity.java
+++ app/src/main/java/net/teamplusbeta/learnhiragana/MainActivity.java
@@ -1,13 +1,9 @@
 package net.teamplusbeta.learnhiragana;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -34,25 +30,5 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
-    }
-
-    public  float getWindowSize(Context context) {
-
-        //画面サイズ取得の準備
-        WindowManager wm = (WindowManager) context.getSystemService(WINDOW_SERVICE);
-        Display disp = wm.getDefaultDisplay();
-
-        float width = 0;
-
-        // AndroidのAPIレベルによって画面サイズ取得方法が異なるので条件分岐
-        if (Integer.valueOf(android.os.Build.VERSION.SDK_INT) < 13) {
-            width = disp.getWidth();
-        } else {
-            Point size = new Point();
-            disp.getSize(size);
-            width = size.x;
-        }
-
-        return width;
     }
 }
